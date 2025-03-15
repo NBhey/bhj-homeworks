f3c2f376fee338b3e911ea03d0fde3711191f5ea
Oganes <og.marckaryan@yandex.ru>
2025-03-15 14:41:14 +0300

    закончил кликер

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,7 +1,28 @@
 const cookie = document.getElementById("cookie");
 const counterClick = document.getElementById("clicker__counter");
 let firstClick = +counterClick.textContent;
+
+
+//Создаем элемент отслеживания клика
+const speedClickElement = document.createElement('div')
+speedClickElement.innerHTML = `Скорость клика: <span id="speed_clicker_counter">0</span>`
+document.querySelector('.clicker img').insertAdjacentElement('beforebegin',speedClickElement)
+
+
+
 cookie.addEventListener("click", () => {
+  changeSpeedClickElement()
   counterClick.textContent = +counterClick.textContent + 1;
   cookie.width == 200 ? (cookie.width = 180) : (cookie.width = 200);
 });
+
+
+let lastClick = Date.now();
+
+function changeSpeedClickElement(){
+  let currentClick = Date.now()
+  let diffClick = (currentClick - lastClick) / 1000;
+  let speedClick = 1 / diffClick;
+  lastClick = currentClick; 
+  document.getElementById('speed_clicker_counter').textContent = speedClick.toFixed(2)
+}
